8b01254a7843d2e10140dd5f6e1faca857252975
kurdi.89 <kurdi.89@hotmail.com>
2019-12-16 01:57:41 +0300

    update

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ jQuery(document).ready(()=>{
 
     // reasons:
     var reasons = ['name', 'age', 'gender', 'height', 'weight', 'activeness','disease']
+    var activenessList = ['very lazy', 'somehow active', 'very active'];
 
 
     // asking function
@@ -80,7 +81,7 @@ jQuery(document).ready(()=>{
 
     // start with the user name : 
     ask('May I have your name please ? ', reasons[0])
-    
+
 
     // conversation : 
     jQuery('form').submit(function( event ) {
@@ -89,26 +90,79 @@ jQuery(document).ready(()=>{
         // edit rpely : 
         reply = $('.reply').val();
         currReason = $('.reply').attr('data-reason');
+
+
+
+
+
+
         /** deal with reply and responese based on reply  */
                 
         // add user reply to the screen: 
         addReply(reply);
         // clear reply form : 
-        clearReply()      
+        clearReply();
+
         // first deal with reply : 
         if(currReason == 'name'){
-            name = reply;
+            name = reply.charAt(0).toUpperCase() + reply.slice(1);
             // second response : 
             respond('Great, got it! ');
             // ask :
             ask('How old are you, ' + name + ' ?', reasons[1])
         }
         else if (currReason == 'age'){
-            age = reply;
-            respond('gotch, ' + name + '!')
+            age = parseInt(reply);
+            respond('gotcha, ' + name + '!')
             ask('May I know, are you a male or female ? ', reasons[2])
         }
-        // scroll : 
+        else if (currReason == 'gender'){
+            reply = reply.toLowerCase().trim();
+            if(reply == "male" || reply == "m"){
+                gender = 'Male'
+                respond('gotcha, ' + name + '!')
+                ask('what is your height in centimeters ? ', reasons[3])
+            }
+            else if(reply == "female" || reply == "f"){
+                gender = 'Female'
+                respond('gotcha, ' + name + '!')
+                ask('how tall are you in centimeters ? ', reasons[3])
+            }
+            else {
+                ask("I couldn't get your gender, May I know, are you a male or female ? ", reasons[2])
+            }
+        }
+        else if (currReason == 'height'){
+            height = parseInt(reply);
+            if(height >= 100 ){
+                respond("That's great, " + name + '!')
+                ask('you are doing good so far '+ name + '! May I know how much do you weight in kilograms ? ', reasons[4])
+            }else {
+                ask("you can't be shorter than 100cm, may I know how tall are you in centimeters ? ", reasons[3])
+            }
+        }
+        else if (currReason == 'weight'){
+            weight = parseInt(reply);
+            respond("Great " + name + '!')
+            ask('you are doing good so far '+ name + '! Can you tell me how active are you during the day : <br> 1: very lazy. <br> 2: somehow active. <br> 3: very active. ', reasons[5])
+        }
+        else if (currReason == 'activeness'){
+            activeness = parseInt(reply);
+            if(Number.isInteger(activeness) && activeness > 0 && activeness <= activenessList.length){
+                respond("me too! I'm " + activenessList[activeness - 1] + '!')
+                // ask('you are doing good so far '+ name + '! Can you tell me how active are you during the day : <br> 1: very lazy. <br> 2: somehow active. <br> 3: very active. ', reasons[5])
+            }
+            else {
+                respond("I'm sorry " + name + " I couldn't get it")
+                ask('Can you tell me how active are you during the day : <br> 1: very lazy. <br> 2: somehow active. <br> 3: very active. <br> You can reply me with either 1, or 2, or 3 ...', reasons[5])
+            }
+        }
+
+        // always scroll : 
+        chatlog.scrollTop(chatlog.prop('scrollHeight'));
+        
+        // auto activate reply area : 
+        $('.reply').focus()
         
     })
     
