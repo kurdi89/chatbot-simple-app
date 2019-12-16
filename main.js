cd0f3f43e8a2a6a27d5b90ce731c96bebeb7f050
kurdi.89 <kurdi.89@hotmail.com>
2019-12-16 04:05:46 +0300

    msg transition

--- main.js
+++ main.js
@@ -33,6 +33,8 @@ jQuery(document).ready(()=>{
         }
         $('.msgClass#' + id).find('.chat').addClass(senderClass);
         $('.msgClass#' + id).find('.user-photo img').attr('src', imagesLink+senderType+'.png');
+        // transition of showing msg : 
+        $('.msgClass#' + id).hide().fadeIn(1000);
     }
 
 
@@ -57,9 +59,19 @@ jQuery(document).ready(()=>{
     
 
     // reasons:
-    var reasons = ['name', 'age', 'gender', 'height', 'weight', 'activeness','disease']
+    var reasons = ['name', 'age', 'gender', 'height', 'weight', 'activeness','disease', 'pregnancy','confirmation']
     var activenessList = ['very lazy', 'somehow active', 'very active'];
+    var diseaseList = ["don't have", 'heart or hight blood pressure', 'diabetes', 'joints', 'kideny']
 
+    // activities :
+    var activities = activenessList.map((e,i)=>{
+        return '[' + i + '] : ' + e + '<br> ';
+    });
+
+    // diseases :
+    var diseases = diseaseList.map((e,i)=>{
+        return '[' + i + '] : ' + e + '<br> ';
+    });
 
     // asking function
     function ask(question, reason){
@@ -126,35 +138,85 @@ jQuery(document).ready(()=>{
             else if(reply == "female" || reply == "f"){
                 gender = 'Female'
                 respond('gotcha, ' + name + '!')
-                ask('how tall are you in centimeters ? ', reasons[3])
+                ask('hmmm, are you pregnant, '+ name + '? <br> - Yes <br> - No ', reasons[7])
+                
             }
             else {
                 ask("I couldn't get your gender, May I know, are you a male or female ? ", reasons[2])
             }
         }
+        else if (currReason == 'pregnancy'){
+            pregnancy = reply.toLowerCase().trim();
+            if(pregnancy == "yes" || pregnancy == "y" || pregnancy == "true" || pregnancy == "I'm" || pregnancy == "I am pregnant"){
+                respond('oh! congratulations in advanced.')
+                ask('how tall are you in centimeters ? ', reasons[3])
+            }
+            else if (pregnancy == "no" || pregnancy == "n" || pregnancy == "no I'm not") {
+                respond("ok " + name + " I get it")
+                ask('how tall are you in centimeters ? ', reasons[3])
+            }else {
+                respond('I am very sorry, ' + name + '!')
+                ask('I could not get it, are you pregnant, '+ name + '? <br> - Yes <br> - No <br> please reply with either yes or no', reasons[7])
+            }
+        }
         else if (currReason == 'height'){
             height = parseInt(reply);
-            if(height >= 100 ){
-                respond("That's great, " + name + '!')
-                ask('you are doing good so far '+ name + '! May I know how much do you weight in kilograms ? ', reasons[4])
-            }else {
-                ask("you can't be shorter than 100cm, may I know how tall are you in centimeters ? ", reasons[3])
+            if(Number.isInteger(weight)){
+                if(height >= 100 ){
+                    respond("That's great, " + name + '!')
+                    ask('you are doing good so far '+ name + '! May I know how much do you weight in kilograms ? ', reasons[4])
+                }else {
+                    ask("you can't be shorter than 100cm, may I know how tall are you in centimeters ? ", reasons[3])
+                }
+            }
+            else {
+                ask('I am sorry, '+ name + '! may I know how tall are you in centimeters ? ', reasons[3])
             }
         }
         else if (currReason == 'weight'){
             weight = parseInt(reply);
-            respond("Great " + name + '!')
-            ask('you are doing good so far '+ name + '! Can you tell me how active are you during the day : <br> 1: very lazy. <br> 2: somehow active. <br> 3: very active. ', reasons[5])
+            if(Number.isInteger(weight)){
+                respond("Great " + name + '!')
+                ask('you are doing good so far '+ name + '! Can you tell me how active are you during the day : <br>' + activities.join('') , reasons[5])
+            }else {
+                ask('I am sorry, '+ name + '! May I know how much do you weight in kilograms ? ', reasons[4])
+            }
         }
         else if (currReason == 'activeness'){
             activeness = parseInt(reply);
-            if(Number.isInteger(activeness) && activeness > 0 && activeness <= activenessList.length){
-                respond("me too! I'm " + activenessList[activeness - 1] + '!')
-                // ask('you are doing good so far '+ name + '! Can you tell me how active are you during the day : <br> 1: very lazy. <br> 2: somehow active. <br> 3: very active. ', reasons[5])
+            if(Number.isInteger(activeness) && activeness > -1 && activeness <= activenessList.length){
+                respond("me too! I'm " + activenessList[activeness] + '!')
+                ask('Do you have any of the following diseases : <br> '+  diseases.join('') , reasons[6])
             }
             else {
                 respond("I'm sorry " + name + " I couldn't get it")
-                ask('Can you tell me how active are you during the day : <br> 1: very lazy. <br> 2: somehow active. <br> 3: very active. <br> You can reply me with either 1, or 2, or 3 ...', reasons[5])
+                ask('Can you tell me how active are you during the day : <br> '+  activities.join('') + ' You can reply me with either 1, or 2, or 3 ...', reasons[5])
+            }
+        }
+        else if (currReason == 'disease'){
+            disease = parseInt(reply);
+            if(Number.isInteger(disease) && disease > -1 && disease <= diseaseList.length){
+                respond("got it !");
+                respond('ok ' + name + ', now I have a great idea of your profile, can you please confirm if these information is correct :')
+                respond('you are ' + age + ' years old '+ gender+ ', and your height is : '+ height + 'cm, you weight about ' + weight + 'kg, you are '+ activenessList[activeness] + ' through the day!')
+                ask('is that correct ?  : <br> [0] : Yes <br> [1] : No', reasons[8])
+            }
+            else {
+                respond("I'm sorry " + name + " I couldn't get it")
+                ask('Can you tell me if you have any of the following diseases : <br> '+  diseases.join('') + ' You can reply me with either 1, or 2, or 3 ...', reasons[6])
+            }
+        }
+        else if (currReason == 'confirmation'){
+            confirmation = reply.toLowerCase().trim();
+            if(confirmation == "yes" || confirmation == "y" || confirmation == "0" || confirmation == "true" || confirmation == "correct" ){
+                respond("Nice!");
+                respond('ok ' + name + ', here are some important information for you :')
+                respond('your BMI is : [] and you need to burn about [] calories per day')
+                respond('here is a list of the exceresizes best for you :')
+            }
+            else if(confirmation == "no" || confirmation == "n" || confirmation == "1" || confirmation == "false" || confirmation == "not" ){
+                respond("I'm very sorry " + name + ", we have to start over")
+                respond("I will referesh the page in 3 seconds and start again")
             }
         }
 
